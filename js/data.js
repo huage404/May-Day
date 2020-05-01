eee49ae3ae192f846325a66a5ddfa8335e9e3a8b
huage404 <1962408497@qq.com>
2020-05-01 10:50:16 +0800

    end

--- js/data.js
+++ js/data.js
@@ -1,128 +1,116 @@
 function postData() {
     let data = {
         'introduction': `平凡的工作，铸就不平凡的责任，为爱为她安个家，“五一黄金周”购房钜惠来袭，凡在南昌楼盘网报名预约看房的用户，都享有额外折扣优惠，购房成功还有机会拿现金红包，最高8888元。`,
-        'houseData': {
-            'apartment': [
-                {
-                    'name': '星悦汇',
-                    'price': '12000',
-                    'intro': '特价风暴，爆款来袭！少量特惠LOFT公寓，位居老城城芯，王府井旁，双地铁繁华之上，坐享未来！',
-                    'discounts': `16888`,
-                    'houseType': '39-50/㎡百变LOFT',
-                    'links': 'http://nc.loupan.com/loupan/7094245.html',
-                    'phone': '400-819-6528,239'
-                },
-                {
-                    'name': '众邦MOHO国际',
-                    'price': '12000',
-                    'intro': '首期5万起抢西客站-省妇保-相伴、双地铁口，首期5万起！',
-                    'discounts': `28888`,
-                    'houseType': '36㎡-50㎡轻奢精装LOFT ',
-                    'links': 'http://nc.loupan.com/loupan/7093515.html',
-                    'phone': '400-819-6528,715'
-                },
-                {
-                    'name': '金地源青年城',
-                    'price': '8500-9000',
-                    'intro': '九龙湖、地铁口、公园畔、带阳台,首付11万起！',
-                    'discounts': `16888`,
-                    'houseType': '38-42㎡通透复式两房',
-                    'links': 'http://nc.loupan.com/loupan/7081601.html',
-                    'phone': '400-819-6528,916'
-                },
-                {
-                    'name': '绿地国博滨江公馆',
-                    'price': '9000-10000',
-                    'intro': '特惠风暴 赣江头排平层SOHO8套特惠8字头！',
-                    'discounts': `16888`,
-                    'houseType': '建面约27-84㎡地铁口LOFT公寓',
-                    'links': 'http://nc.loupan.com/loupan/7019250.html',
-                    'phone': '400-819-6528,666'
-                }
-            ],
-            'residence': [
-                {
-                    'name': '绿地儒乐星镇',
-                    'price': '12000-12500',
-                    'intro': '清零计划 南大附小落户赣江新区 仅此50套！',
-                    'discounts': `16888`,
-                    'houseType': '100-120㎡精装修高层',
-                    'links': 'http://nc.loupan.com/loupan/7094585.html',
-                    'phone': '400-819-6528,967'
-                },
-                {
-                    'name': '赣电贵府',
-                    'price': '8584',
-                    'intro': '引爆全城，一惠到底！臻品1#楼5.1日应市加推！10套好房一口价!',
-                    'discounts': `16888`,
-                    'houseType': '89-108m²金妆瞰湖美宅',
-                    'links': 'http://nc.loupan.com/loupan/7103344.html',
-                    'phone': '400-819-6528,498'
-                },
-                {
-                    'name': '江铃时代城',
-                    'price': '12500',
-                    'intro': '地铁4号线近在咫尺，坐拥九龙湖核心地段，带学区',
-                    'discounts': `16888`,
-                    'houseType': '89~116㎡金品高层',
-                    'links': 'http://nc.loupan.com/loupan/7044587.html',
-                    'phone': '400-819-6528,221'
-                },
-                {
-                    'name': '金科集美九龙湖',
-                    'price': '13600',
-                    'intro': '五一特惠提前抢 单价13403元/㎡起抢金工高层/洋房！',
-                    'discounts': `16888`,
-                    'houseType': '95-123㎡金工高层/洋房',
-                    'links': 'http://nc.loupan.com/loupan/7094018.html',
-                    'phone': '400-819-6528,861'
-                }
-            ],
-            'villa': [
-                {
-                    'name': '红星桃花源筑',
-                    'price': '7000',
-                    'intro': '首付2成，一梯俩户，户户通透，超高得房率，不限购!',
-                    'discounts': `16888`,
-                    'houseType': '85-95㎡轻奢洋房',
-                    'links': 'http://nc.loupan.com/loupan/7044606.html',
-                    'phone': '400-819-6528,957'
-                },
-                {
-                    'name': '泰禾南昌院子',
-                    'price': '15000-25000',
-                    'intro': '一线临湖，山湖美景尽收眼底。新品上下院楼栋火热销售中!',
-                    'discounts': `16888`,
-                    'houseType': '建面约160-220㎡新品大宅',
-                    'links': 'http://nc.loupan.com/loupan/7074812.html',
-                    'phone': '400-819-6528,505'
-                },
-                {
-                    'name': '绿创中心',
-                    'price': '9000-10000',
-                    'intro': '中式合院别墅在售 总价190万起！民水民电！',
-                    'discounts': `16888`,
-                    'houseType': '179㎡~270㎡中式三层合院',
-                    'links': 'http://nc.loupan.com/loupan/7044557.html',
-                    'phone': '400-819-6528,481'
-                },
-                {
-                    'name': '绿地安南小镇',
-                    'price': '5700',
-                    'intro': '九龙湖开车20分钟，价格三分之一，不限购！湖心岛云栖墅强势来袭!',
-                    'discounts': `16888`,
-                    'houseType': '78-120㎡瞰景高层',
-                    'links': 'http://nc.loupan.com/loupan/7092910.html',
-                    'phone': '400-819-6528,573'
-                }
-            ],
-            'shop': [
-                { 'name': '绿滋肴庙街' },
-                { 'name': '年代星辰' },
-                { 'name': '恒天时尚中心' },
-                { 'name': '新宇盛世广场' }
-            ]
-        }
+        'houseData': [
+            {
+                'name': '星悦汇',
+                'price': '12000',
+                'intro': '特价风暴，爆款来袭！少量特惠LOFT公寓，位居老城城芯，王府井旁，双地铁繁华之上，坐享未来！',
+                'discounts': `16888`,
+                'houseType': '39-50/㎡百变LOFT',
+                'links': 'http://nc.loupan.com/loupan/7094245.html',
+                'phone': '400-819-6528,239'
+            },
+            {
+                'name': '众邦MOHO国际',
+                'price': '12000',
+                'intro': '首期5万起抢西客站-省妇保-相伴、双地铁口，首期5万起！',
+                'discounts': `28888`,
+                'houseType': '36㎡-50㎡精装LOFT ',
+                'links': 'http://nc.loupan.com/loupan/7093515.html',
+                'phone': '400-819-6528,715'
+            },
+            {
+                'name': '金地源青年城',
+                'price': '8500-9000',
+                'intro': '九龙湖、地铁口、公园畔、带阳台,首付11万起！',
+                'discounts': `16888`,
+                'houseType': '38-42㎡复式两房',
+                'links': 'http://nc.loupan.com/loupan/7081601.html',
+                'phone': '400-819-6528,916'
+            },
+            {
+                'name': '绿地国博滨江公馆',
+                'price': '9000-10000',
+                'intro': '特惠风暴 赣江头排平层SOHO8套特惠8字头！',
+                'discounts': `16888`,
+                'houseType': '27-84㎡LOFT公寓',
+                'links': 'http://nc.loupan.com/loupan/7019250.html',
+                'phone': '400-819-6528,666'
+            },
+            {
+                'name': '绿地儒乐星镇',
+                'price': '12000-12500',
+                'intro': '清零计划 南大附小落户赣江新区 仅此50套！',
+                'discounts': `16888`,
+                'houseType': '100-120㎡精装修高层',
+                'links': 'http://nc.loupan.com/loupan/7094585.html',
+                'phone': '400-819-6528,967'
+            },
+            {
+                'name': '赣电贵府',
+                'price': '8584',
+                'intro': '引爆全城，一惠到底！臻品1#楼5.1日应市加推！10套好房一口价!',
+                'discounts': `16888`,
+                'houseType': '89-108m²瞰湖美宅',
+                'links': 'http://nc.loupan.com/loupan/7103344.html',
+                'phone': '400-819-6528,498'
+            },
+            {
+                'name': '江铃时代城',
+                'price': '12500',
+                'intro': '地铁4号线近在咫尺，坐拥九龙湖核心地段，带学区',
+                'discounts': `16888`,
+                'houseType': '89~116㎡金品高层',
+                'links': 'http://nc.loupan.com/loupan/7044587.html',
+                'phone': '400-819-6528,221'
+            },
+            {
+                'name': '金科集美九龙湖',
+                'price': '13600',
+                'intro': '五一特惠提前抢 单价13403元/㎡起抢金工高层/洋房！',
+                'discounts': `16888`,
+                'houseType': '95-123㎡金工高层',
+                'links': 'http://nc.loupan.com/loupan/7094018.html',
+                'phone': '400-819-6528,861'
+            },
+            {
+                'name': '红星桃花源筑',
+                'price': '7000',
+                'intro': '首付2成，一梯俩户，户户通透，超高得房率，不限购!',
+                'discounts': `16888`,
+                'houseType': '85-95㎡轻奢洋房',
+                'links': 'http://nc.loupan.com/loupan/7044606.html',
+                'phone': '400-819-6528,957'
+            },
+            {
+                'name': '泰禾南昌院子',
+                'price': '15000-25000',
+                'intro': '一线临湖，山湖美景尽收眼底。新品上下院楼栋火热销售中!',
+                'discounts': `16888`,
+                'houseType': '160-220㎡新品大宅',
+                'links': 'http://nc.loupan.com/loupan/7074812.html',
+                'phone': '400-819-6528,505'
+            },
+            {
+                'name': '绿创中心',
+                'price': '9000-10000',
+                'intro': '中式合院别墅在售 总价190万起！民水民电！',
+                'discounts': `16888`,
+                'houseType': '179㎡~270㎡中式合院',
+                'links': 'http://nc.loupan.com/loupan/7044557.html',
+                'phone': '400-819-6528,481'
+            },
+            {
+                'name': '绿地安南小镇',
+                'price': '5700',
+                'intro': '九龙湖开车20分钟，价格三分之一，不限购！湖心岛云栖墅强势来袭!',
+                'discounts': `16888`,
+                'houseType': '78-120㎡瞰景高层',
+                'links': 'http://nc.loupan.com/loupan/7092910.html',
+                'phone': '400-819-6528,573'
+            }
+        ]
     }
     return data;
 }
